3e3279149d6bae7b6e141abb530cb6f0567e6f8d
Kacper <144324382+Kacperrrr2@users.noreply.github.com>
2024-04-09 23:52:50 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+# ANAGRAMY
+# wiersze=open('anagram.txt','r')
+# tab=[]
+# tab_wszystkich=[]
+# sprawdz=True
+# for wiersz in wiersze:
+#     tab.append(wiersz.strip().split())
+#
+# for i in range(len(tab)):
+#     for j in range(len(tab[i])-1):
+#         if len(tab[i][j])!=len(tab[i][j+1]):
+#             sprawdz=False
+#     if sprawdz==True:
+#         tab_wszystkich.append(tab[i])
+#     sprawdz=True
+# print(tab_wszystkich)
+#
+
+#
+# wiersze=open('anagram.txt','r')
+# tab=[]
+# tab_wszystkich=[]
+# sprawdz=True
+# for wiersz in wiersze:
+#     tab.append(wiersz.strip().split())
+#
+# for i in range(len(tab)):
+#     for j in range(len(tab[i])-1):
+#         if sorted(tab[i][j])!=sorted(tab[i][j+1]) :
+#             sprawdz=False
+#     if sprawdz==True:
+#         tab_wszystkich.append(tab[i])
+#     sprawdz=True
+# print(tab_wszystkich)
+
